4fb26f57a7c05aa6162117c66109a9da07fb6c84
Christopher Robinson <chrispaul_robinson@hotmail.com>
2021-09-15 21:46:29 -0500

    Finish success screen

--- src/routes/success.js
+++ src/routes/success.js
@@ -1,11 +1,48 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import PropTypes from "prop-types";
+import Card from "../components/card";
+import useMediaQuery from "../hooks/useMediaQuery";
+import styled from "@emotion/styled";
+import CheckMark from "../assets/check.svg";
+
+const SuccessCont = styled.div`
+  /* Layout */
+  height: 18rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  justify-items: center;
+
+  /* Presentation */
+
+  img {
+    height: 8rem;
+    width: 8rem;
+  }
+
+  h4 {
+    color: grey;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 1.4rem;
+  }
+`;
 
 const Success = (props) => {
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
+
   return (
     <div>
-      <p>This is the success route</p>
+      <Card width={isDesktop ? "50rem" : "20rem"}>
+        <SuccessCont className="success-cont">
+          <h2>Purchase success!</h2>
+          <img src={CheckMark} alt="Check mark image" />
+          <h4>Access Link Sent To Email</h4>
+        </SuccessCont>
+      </Card>
     </div>
   );
 };
